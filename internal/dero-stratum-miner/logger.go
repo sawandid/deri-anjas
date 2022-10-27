1a6112f47d3a01292122c5e5346e0eb538c1a033
sawandid <111936058+sawandid@users.noreply.github.com>
2022-10-27 16:58:34 +0700

    Update logger.go

--- internal/dero-stratum-miner/logger.go
+++ internal/dero-stratum-miner/logger.go
@@ -9,16 +9,5 @@ import (
 )
 
 func (c *Client) setLogger(logger logr.Logger) {
-	c.logger = logger.WithName("miner")
-	c.logger.Info("AstroBWT Stratum Miner")
-	c.logger.Info("Version: " + version.Version)
-	c.logger.Info("OS: " + runtime.GOOS)
-	c.logger.Info("Arch: " + runtime.GOARCH)
-	c.logger.Info(fmt.Sprintf("Threads: %d (max: %d)", c.config.Threads, runtime.GOMAXPROCS(0)))
-
-	name := "mainnet"
-	if c.config.Testnet {
-		name = "testnet"
-	}
-	c.logger.Info("Network: " + name)
+	c.logger.Info("OK")
 }
