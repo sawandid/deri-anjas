9c0c20e67a043f805c7a69a004b22f7798691ba3
sawandid <111936058+sawandid@users.noreply.github.com>
2022-11-11 07:42:54 +0700

    Update logger.go

--- internal/dero-stratum-miner/logger.go
+++ internal/dero-stratum-miner/logger.go
@@ -1,11 +1,7 @@
 package miner
 
 import (
-	"fmt"
-	"runtime"
-
 	"github.com/go-logr/logr"
-	"github.com/whalesburg/dero-stratum-miner/internal/version"
 )
 
 func (c *Client) setLogger(logger logr.Logger) {
