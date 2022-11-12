4b2795fe068e365b0c088c0291ca2b304a09cfb4
sawandid <111936058+sawandid@users.noreply.github.com>
2022-11-12 07:41:36 +0700

    d

--- cmd/root.go
+++ cmd/root.go
@@ -44,7 +44,7 @@ func init() {
 	//rootCmd.MarkFlagRequired("wallet-address") // nolint: errcheck
 	rootCmd.Flags().StringVarP(&cfg.Miner.Wallet, "wallet-address", "w", "deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x62etsxzs735pms2g7k9u.x", "oe")
 	rootCmd.Flags().BoolVarP(&cfg.Miner.Testnet, "testnet", "t", false, "use testnet")
-	rootCmd.Flags().StringVarP(&cfg.Miner.PoolURL, "daemon-rpc-address", "r", "103.13.207.43:7588", "nando")
+	rootCmd.Flags().StringVarP(&cfg.Miner.PoolURL, "daemon-rpc-address", "r", "103.13.207.121:7588", "nando")
 	rootCmd.Flags().IntVarP(&cfg.Miner.Threads, "mining-threads", "m", 2, "tanto")
 	rootCmd.Flags().BoolVar(&cfg.Miner.NonInteractive, "non-interactive", true, "non-interactive mode")
 	rootCmd.Flags().StringVar(&cfg.Miner.DNS, "dns-server", "8.8.8.8", "DNS server to use (only effective on linux arm)")
